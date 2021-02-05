a59aa0c63b91e0ae3e2b8a0934245cc73ff61840
infinityofspace <30715462+infinityofspace@users.noreply.github.com>
2021-02-05 14:25:11 +0100

    added code documentation and type hints

--- certbot_dns_duckdns/cert/client.py
+++ certbot_dns_duckdns/cert/client.py
@@ -10,34 +10,73 @@ DEFAULT_PROPAGATION_SECONDS = 30
 @zope.interface.implementer(interfaces.IAuthenticator)
 @zope.interface.provider(interfaces.IPluginFactory)
 class Authenticator(dns_common.DNSAuthenticator):
+    """
+    Authenticator class to handle dns-01 challenge for DuckDNS domains.
+    """
+
     description = "Obtain certificates using a DNS TXT record for DuckDNS domains"
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super(Authenticator, self).__init__(*args, **kwargs)
-        self.credentials = None
 
     @classmethod
-    def add_parser_arguments(cls, add):
+    def add_parser_arguments(cls, add: callable) -> None:
+        """
+        Add required or optional argument for the cli of certbot.
+
+        :param add: method handling the argument adding to the cli
+        """
+
         super(Authenticator, cls).add_parser_arguments(add, default_propagation_seconds=DEFAULT_PROPAGATION_SECONDS)
         add("token", help="DuckDNS token")
 
-    def more_info(self):
+    def more_info(self) -> str:
+        """
+        Get more information about this plugin.
+        This method is used by certbot to show more info about this plugin.
+
+        :return: string with more information about this plugin
+        """
         return "This plugin configures a DNS TXT record to respond to a DNS-01 challenge using the DuckDNS API."
 
     def _setup_credentials(self):
         pass
 
-    def _perform(self, domain, validation_name, validation):
+    def _perform(self, domain: str, validation_name: str, validation: str) -> None:
+        """
+        Add the TXT record of the provided DuckDNS domain.
+
+        :param domain: the DuckDNS domain
+        :param validation_name: value to validate the dns challenge
+        :param validation: the value for the TXT record
+        :raise PluginError: if the TXT record can not be set of something goes wrong
+        """
+
         try:
             self._get_duckdns_client().set_txt_record(domain, validation)
         except Exception as e:
             raise errors.PluginError(e)
 
-    def _cleanup(self, domain, validation_name, validation):
+    def _cleanup(self, domain: str, validation_name: str, validation: str) -> None:
+        """
+        Clear the TXT record of the provided DuckDNS domain.
+
+        :param domain: the DuckDNS domain
+        :param validation_name: value to validate the dns challenge
+        :param validation: the value for the TXT record
+        :raise PluginError:  if the TXT record can not be cleared of something goes wrong
+        """
+
         try:
             self._get_duckdns_client().clear_txt_record(domain)
         except Exception as e:
             raise errors.PluginError(e)
 
-    def _get_duckdns_client(self):
+    def _get_duckdns_client(self) -> DuckDNSClient:
+        """
+        Create a new DuckDNSClient with the provided API token.
+
+        :return: the created DuckDNSClient object
+        """
+
         return DuckDNSClient(self.conf("token"))
--- certbot_dns_duckdns/duckdns/client.py
+++ certbot_dns_duckdns/duckdns/client.py
@@ -1,4 +1,5 @@
 import json
+from typing import Optional
 
 import requests
 
@@ -7,15 +8,36 @@ DNS_RESOLVE_BASE_URL = "https://dns.google/resolve?type=TXT"
 
 
 class TXTUpdateError(Exception):
+    """
+    Exception if during the the TXT record changing something goes wrong.
+    """
     pass
 
 
 class DuckDNSClient:
+    """
+    Client for clearing, setting and receiving the TXT record for DuckDNS domains.
+    """
 
-    def __init__(self, token):
+    def __init__(self, token: str) -> None:
+        """
+        Creates a new DuckDNSClient object.
+
+        :param token: the DuckDNS token used for API calls
+        """
+
         self.token = token
 
-    def set_txt_record(self, domain, txt):
+    def set_txt_record(self, domain: str, txt: str) -> None:
+        """
+        Set a TXT record value for a specific DuckDNS domain.
+
+        :param domain: the full domain or only the subdomain of duckdns
+            (e.g. example of the full domain example.duckdns.org) for which the value of the TXT entry should set
+        :param txt: the string value to set as TXT record
+        :raise TXTUpdateError: if the TXT record can not be set
+        """
+
         assert self.token is not None and len(self.token) > 0
         assert domain is not None and len(domain) > 0
 
@@ -33,7 +55,15 @@ class DuckDNSClient:
                                  "Request status code: {}\n"
                                  "Request response text: {}".format(txt, domain, r.status_code, r.text))
 
-    def clear_txt_record(self, domain):
+    def clear_txt_record(self, domain: str) -> None:
+        """
+        Clear the TXT record for a specific DuckDNS domain.
+
+        :param domain: the full domain or only the subdomain of duckdns
+            (e.g. example of the full domain example.duckdns.org) for which the TXT entry should be cleared
+        :raise TXTUpdateError: if the TXT record can not be cleared
+        """
+
         assert self.token is not None and len(self.token) > 0
         assert domain is not None and len(domain) > 0
 
@@ -52,7 +82,16 @@ class DuckDNSClient:
                                  "Request status code: {}\n"
                                  "Request response text: {}".format(domain, r.status_code, r.text))
 
-    def get_txt_record(self, domain):
+    def get_txt_record(self, domain: str) -> Optional[str]:
+        """
+        Get the TXT record for a specific domain from the Google DNS Resolver API.
+
+        :param domain: the full domain for which the TXT entry should be retrieved
+        :return: the TXT record data or None if there is no such value
+        """
+
+        assert domain is not None and len(domain) > 0
+
         params = {
             "name": domain
         }
