2d1ae6eba92d54aafd384a31b8ec95fb4532f4dd
infinityofspace <30715462+infinityofspace@users.noreply.github.com>
2024-11-07 21:11:52 +0100

    added support for python 3.13

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "License :: OSI Approved :: MIT License",
         "Topic :: Security",
         "Topic :: Internet :: WWW/HTTP",
